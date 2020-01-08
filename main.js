ee40d4cf8f42781681b67eec28b4291bae85af40
Nguyen Hai-Son <nhson17@apcs.vn>
2020-01-08 20:47:31 +0700

    Final version

--- main.js
+++ main.js
@@ -2,28 +2,35 @@ const convert = require('./convert')
 const subtitlevtt = require('./subtitle-vtt')
 const subtitlesrt = require('./subtitle-srt')
 
+// var uploadfile = document.getElementById("fileToUpload")
+// var convertProcess = document.getElementById('convertProcess')
+// var subSrt = document.getElementById('s1')
+// var subVtt = document.getElementById('s2')
+
+//subSrt.innerHTML = "Download"
+
 const filename = process.argv[2]
 const typeSubtitle = process.argv[3]
 const language = process.argv[4]
 
+
 function main() {
-convert.speechToText(filename, language).then(result => {
-
-    console.log(result)
-try {
-    if (typeSubtitle == 'srt') {
-       subtitlesrt.write(result)
+    convert.speechToText(filename, language).then(result => {
+    
+        console.log(result)
+    try {
+        if (typeSubtitle === 'srt') {
+           subtitlesrt.write(result)
+        }
+        else if (typeSubtitle === 'vtt') {
+           subtitlevtt.write(result)
+        }
+        else if (typeSubtitle != 'srt' || typeSubtitle != 'vtt') {
+        throw "Subtitle Type NOT FOUND";
     }
-    if (typeSubtitle == 'vtt') {
-       subtitlevtt.write(result)
-    }
-    else if (typeSubtitle !== 'srt' || typeSubtitle !== 'vtt') 
-    throw console.error("Subtitle Type NOT FOUND");
-    }
-catch(e) {console.log(e)}  
-}
-)
-}
-
+        }
+    catch(e) {console.log(e)}  
+    })}
+    
 main()
 // node main.js filename subtype language
--- upload.php
+++ upload.php
@@ -1,35 +1,27 @@
 <?php
-$target_dir = "uploads/";
+$target_dir = "./audio/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
+$audioFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
 // Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
+
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
+
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+if($audioFileType != "wav" ) {
+    echo "Sorry, only WAV sound files are allowed.";
     $uploadOk = 0;
 }
+
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
